79d51b1f869aa956e29576f0a2ba016eefa650bf
Dirk Grappendorf <d.grappendorf@web.de>
2012-10-24 15:45:16 +0200

    Option for running Maven in batch mode Gradle support

--- build.sh
+++ build.sh
@@ -3,40 +3,54 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd ${DIR}
 
 optClean=
 optStage=1
 optFormat=
-optSite=
 optSettings=
 optMavenSettings=
+optBatchMode=
+optUseGradle=
 
-shortOptions=c1h
-longOptions=format,site,settings:
+shortOptions=c1gh
+longOptions=format,settings:,batchmode,gradle
 
-options=("$(getopt -u -o $shortOptions --long $longOptions -- $@)")
+options=("$(getopt -u -o ${shortOptions} --long ${longOptions} -- $@)")
 
 function processOptions
 {
 	while [ $# -gt 0 ]
 	do
 		case $1 in
-			-c) optClean=1;;
-			-1) optStage=1;;
-			--format) optFormat=1;;
-			--site) optSite=1;;
-			--settings) shift
+			-c)
+				optClean=1
+				;;
+			-1)
+				optStage=1
+				;;
+			--format)
+				optFormat=1
+				;;
+			--settings)
+				shift
 				optSettings="--settings $1"
 				optMavenSettings="-s $HOME/.m2/$1-settings.xml"
+				optGradleSettings="$1"
+				;;
+			--batchmode)
+			        optBatchMode=1;;
+			-g|--gradle)
+				optUseGradle=1
 				;;
 			-h) printf "Usage: %s: [options]\n" $0
 			    printf "Options:\n"
 			    printf " -c                              Perform a clean build\n"
 			    printf " -1                              Start from stage 1\n"
 			    printf " --format                        Format the source code\n"
-			    printf " --site                          Update the project web site\n"
 			    printf " --settings                      Alternate Maven settings file\n"
+			    printf " --batchmode                     Run maven in batch mode\n"
+			    printf " -g,--gradle                     Use Grade instead of Maven\n"
 			    printf "                                 (Leave out the path and the extension)\n"
 			    printf " -h                              Print this help\n"
 			    exit 0
@@ -46,41 +60,57 @@ function processOptions
 	done
 }
 
-processOptions $options
+processOptions ${options}
 
+MVN="mvn"
 if [ ! -z "$optMavenSettings" ]
 then
-	MVN="mvn $optMavenSettings"
-else
-	MVN="mvn"
+	MVN="$MVN $optMavenSettings"
+fi
+if [ ! -z "$optBatchMode" ]
+then
+	MVN="$MVN -B"
 fi
 
+BUILD="./build.sh"
 if [ ! -z "$optSettings" ]
 then
-	BUILD="./build.sh $optSettings"
+	BUILD="$BUILD $optSettings"
+fi
+if [ ! -z "$optBatchMode" ]
+then
+	BUILD="$BUILD $optBatchMode"
+fi
+
+if [ ! -z "${optGradleSettings}" ]
+then
+	GRADLE="gradle ${optGradleSettings}"
 else
-	BUILD="./build.sh"
+	GRADLE="gradle"
 fi
 
 shift $(($OPTIND - 1))
 
-if [ ! -z "$optFormat" ]
+if [ ! -z "${optFormat}" ]
 then
-	$MVN java-formatter:format
-	$MVN license:format
+	${MVN} java-formatter:format
+	${MVN} license:format
 	exit 0
 fi
 
-if [ ! -z "$optSite" ]
+if [ ! -z "${optClean}" ]
 then
-	$MVN site:site
-	$MVN site:deploy
-	exit 0
+	if [ ! -z "${optUseGradle}" ]
+	then
+		${GRADLE} clean
+	else
+		${MVN} clean
+	fi
 fi
 
-if [ ! -z "$optClean" ]
+if [ ! -z "${optUseGradle}" ]
 then
-	$MVN clean
+	${GRADLE} install
+else
+	${MVN} install
 fi
-
-$MVN install
